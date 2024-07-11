73d80f49e095e958a75f17f89679957502185178
muralidhar-nalabothula <muralidharrsvm7@gmail.com>
2024-07-11 18:58:20 +0200

    Fortran interface working

--- src/test/test_herm_fortran.f90
+++ src/test/test_herm_fortran.f90
@@ -5,51 +5,64 @@ program main
   implicit none
 
   integer :: ierr, size, my_rank
-  integer(c_int) :: Grows, Gcols, ProcX, ProcY, blockX, blockY
-  type(c_ptr) :: mpicxt, Matrix, Matrix_Z
+  integer(YDIAGO_INT) :: Grows, Gcols, ProcX, ProcY, blockX, blockY
+  type(c_ptr) :: mpicxt, Matrix, evecs
   real(8) :: start, end
-  integer(c_int) :: ele_this_cpu, ele_rem, shift, gele, ir, jc
-  integer(c_int) :: iloc, jloc, iglob, jglob
-  complex(C_FLOAT) :: tmp11
-  logical :: loc_correct, elem_pass
-  integer(c_int) :: nfound
-  complex(C_COMPLEX), allocatable :: eig_vals(:)
-  integer(c_int) :: i, error
-  real(C_FLOAT), parameter :: refvals(16) = [ -1.01363347e+01_C_FLOAT, -8.85451267e+00_C_FLOAT, -4.73670736e+00_C_FLOAT, -4.41315701e+00_C_FLOAT, &
-                                              -9.16672860e-01_C_FLOAT, -1.27597110e-01_C_FLOAT, -2.06397617e-02_C_FLOAT, -7.57303011e-04_C_FLOAT, &
-                                              7.83462424e-04_C_FLOAT, 1.85921577e-02_C_FLOAT, 2.40734728e-01_C_FLOAT, 6.46603001e-01_C_FLOAT, &
-                                              4.23501051e+00_C_FLOAT, 4.78593026e+00_C_FLOAT, 7.46607179e+00_C_FLOAT, 9.85701459e+00_C_FLOAT ]
+  integer(YDIAGO_INT) :: ele_this_cpu, ele_rem, shift, gele, ir, jc
+  complex(YDIAGO_FLOAT) :: tmp11
+  integer(YDIAGO_INT) :: neig_found
+  complex(YDIAGO_CMPLX), target, allocatable :: eig_vals(:)
+  complex(YDIAGO_CMPLX), target, allocatable :: eig_vectors(:,:)
+  integer(YDIAGO_INT) :: i, error
+  integer(YDIAGO_INT) :: desc(9)
 
   call MPI_Init(ierr)
-  call setbuf(stdout, 0)
+
   call MPI_Comm_size(MPI_COMM_WORLD, size, ierr)
+  IF (size .LT. 4) THEN
+    WRITE(*,'(A)') 'At least 4 cpus are required to run this program'
+    CALL MPI_Abort(MPI_COMM_WORLD, -1, ierr)
+  END IF
   call MPI_Comm_rank(MPI_COMM_WORLD, my_rank, ierr)
 
-  Grows = 16_c_int
-  Gcols = 16_c_int
-  ProcX = 2_c_int
-  ProcY = 2_c_int
-  blockX = 8_c_int
-  blockY = 8_c_int
+  Grows  = 16
+  Gcols  = 16
+  ProcX  = 2
+  ProcY  = 2
+  blockX = 8
+  blockY = 8
+  
+  ! Always check the return errors, The library almost never exits 
+  !(unless it is a fatal error) but gracefull exits. 
+  !This means, the functions might fail without quiting the program.
 
-  mpicxt = BLACScxtInit('R', MPI_COMM_WORLD, ProcX, ProcY)
+  ! First we need to create a blacs grid 
+  mpicxt = BLACScxtInit_Fortran('R', MPI_COMM_WORLD, ProcX, ProcY)
+  ! Here I used rowmajor layout, one can also use 'C' which is
+  ! Coloumn major. This is matter of taste.
 
-  call MPI_Barrier(MPI_COMM_WORLD, ierr)
-  start = MPI_Wtime()
+  ! Let us create a block cyclic matrix which we want to diagonalize in memory
   Matrix = init_D_Matrix(Grows, Gcols, blockX, blockY, mpicxt)
-  call MPI_Barrier(MPI_COMM_WORLD, ierr)
-  end = MPI_Wtime()
-
-  if (my_rank == 0) then
-    print *, "Init D mat : ", end - start
+  ! Check is Matrix is properly initiated
+  if (.not. c_associated(Matrix)) then
+    call MPI_Abort(MPI_COMM_WORLD, 1, ierr)
   end if
+  
+  ! Similarly Create a distrbuted matrix for eigenvectors. 
+  ! Note the code only returns Right eigenvectors even incase of non-TDA
+  evecs = init_D_Matrix(Grows, Gcols, blockX, blockY, mpicxt)
+  ! check if evecs are initiated properly
+  ! Eigenvalues are stored in columns of the distributed matrix i.e 
+  ! evecs[:,i] represents the ith eigenvector
 
-  if (.not. associated(Matrix)) then
+
+  if (.not. c_associated(evecs)) then
     call MPI_Abort(MPI_COMM_WORLD, 1, ierr)
   end if
 
   error = 0
 
+  !! Not so important. Just seting up few things 
   ele_this_cpu = (Grows * Gcols) / size
   ele_rem = mod(Grows * Gcols, size)
   shift = ele_this_cpu * my_rank
@@ -60,147 +73,168 @@ program main
   else
     shift = shift + ele_rem
   end if
+  
+  !! Now Let us now fill the matrix with random values. 
+  !! each CPU has different elements in the and wants to set
+  ! the distributed matrix (Matrix)
+  ! Yambo calls this as Matrix folding
 
-  call MPI_Barrier(MPI_COMM_WORLD, ierr)
-  start = MPI_Wtime()
+  ! Initiate the folding procedure (of element setup process)
   error = initiateSetQueue(Matrix, ele_this_cpu)
-  call MPI_Barrier(MPI_COMM_WORLD, ierr)
-  end = MPI_Wtime()
-  if (my_rank == 0) then
-    print *, "Init set : ", end - start
-  end if
-
+  ! each cpus is setting ele_this_cpu number of elements to the
+  ! Distributed matrix.
   if (error /= 0) then
     call MPI_Abort(MPI_COMM_WORLD, 1, ierr)
   end if
 
-  call MPI_Barrier(MPI_COMM_WORLD, ierr)
-  start = MPI_Wtime()
-
-  do i = 0, ele_this_cpu-1
-    gele = i + shift
-    ir = gele / Gcols
-    jc = mod(gele, Gcols)
-    tmp11 = csin(gele + cmplx(0.0_C_FLOAT, cos(gele)))
-
+  do i = 1, ele_this_cpu
+    gele = i + shift-1
+    ir = gele / Gcols  + 1
+    jc = mod(gele, Gcols) + 1
+    tmp11 = csin(gele + cmplx(0.0_YDIAGO_FLOAT, cos(real(gele))))
     gele = ir + jc * Gcols
-    tmp11 = tmp11 + conjg(csin(gele + cmplx(0.0_C_FLOAT, cos(gele))))
+    tmp11 = tmp11 + conjg(csin(gele + cmplx(0.0_YDIAGO_FLOAT, cos(real(gele)))))
+    
+    !! Note set the (ir,jc) element to tmp11
+    error = DMatSet_Fortran(Matrix, ir, jc, tmp11)
 
-    error = DMatSet(Matrix, ir, jc, tmp11)
+    !! Check the error
     if (error /= 0) then
       call MPI_Abort(MPI_COMM_WORLD, 1, ierr)
     end if
   end do
 
-  call MPI_Barrier(MPI_COMM_WORLD, ierr)
-  end = MPI_Wtime()
-  if (my_rank == 0) then
-    print *, "Setting mat : ", end - start
-  end if
-
-  call MPI_Barrier(MPI_COMM_WORLD, ierr)
-  start = MPI_Wtime()
-
+  ! Once setting up the elements is finished, we finilize the setting procedure
+  ! by call the processSetQueue function
   error = ProcessSetQueue(Matrix)
+  ! After this, we sucessfull set the elements of the distributed matrix.
+  ! Now we are good to go for diagonalization procedure.
 
-  end = MPI_Wtime()
-  if (my_rank == 0) then
-    print *, "SetQueue Process : ", end - start
-  end if
-
+  ! Check the error
   if (error /= 0) then
     call MPI_Abort(MPI_COMM_WORLD, 1, ierr)
   end if
 
-  loc_correct = .true.
-
-  ! Check if the elements in the local block are correct
-  do iloc = 0, dismat%ldims(1) - 1
-    do jloc = 0, dismat%ldims(2) - 1
-      iglob = INDXL2G(iloc, dismat%block_size(1), dismat%pids(1), 0, dismat%pgrid(1))
-      jglob = INDXL2G(jloc, dismat%block_size(2), dismat%pids(2), 0, dismat%pgrid(2))
-
-      gele = iglob * Gcols + jglob
-      tmp11 = csin(gele + cmplx(0.0_C_FLOAT, cos(gele)))
-      gele = jglob * Gcols + iglob
-      tmp11 = tmp11 + conjg(csin(gele + cmplx(0.0_C_FLOAT, cos(gele))))
-
-      if (abs(tmp11 - dismat%data(iloc * dismat%lda(1) + jloc * dismat%lda(2))) > 1.0e-6_C_FLOAT) then
-        loc_correct = .false.
-      end if
-    end do
-  end do
-
-  call MPI_Reduce(loc_correct, elem_pass, 1, MPI_LOGICAL, MPI_LOR, 0, MPI_COMM_WORLD, ierr)
-
-  if (my_rank == 0) then
-    if (elem_pass) then
-      print *, "Local elements set correctly. Passed"
-    else
-      print *, "Local elements NOT set correctly. Failed"
-    end if
-  end if
-
-  Matrix_Z = init_D_Matrix(Grows, Gcols, blockX, blockY, mpicxt)
-
-  if (.not. associated(Matrix_Z)) then
-    call MPI_Abort(MPI_COMM_WORLD, 1, ierr)
-  end if
-
+  ! Now allocate the eigenvalues.
   allocate(eig_vals(Grows))
+  eig_vals = 1
+  ! Check if sucessfull allocated
   if (.not. allocated(eig_vals)) then
     call MPI_Abort(MPI_COMM_WORLD, 1, ierr)
   end if
 
-  call MPI_Barrier(MPI_COMM_WORLD, ierr)
-  start = MPI_Wtime()
   if (my_rank == 0) then
     print *, "Diagonalization......."
   end if
 
-#ifdef TEST_ELPA
-  call Heev_Elpa(Matrix, eig_vals, Matrix_Z, -1, 2, null, 1)
-#else
-  call Geev(Matrix, eig_vals, null, Matrix_Z)
-  nfound = 0
-#endif
-
-  call MPI_Barrier(MPI_COMM_WORLD, ierr)
-  end = MPI_Wtime()
+  !! Ignore. testing descriptor function
+  error = set_descriptor(Matrix, desc)
   if (my_rank == 0) then
-    print *, "Diagonalization time : ", end - start
+    do i = 1, 9 
+      print *, desc(i) 
+    end do
   end if
 
-#ifdef PRINT_EIGS
+
+  ! Note call the solver you need. Here we are calling the hermitian solver
+  ! This is overwritten by the number of eigenvalues found (only for scalapack)
+  neig_found = 0
+  
+  ! Call the diagonalization funtion 
+  
+  ! Scalapack TDA 
+  error = Heev(Matrix, 'L', c_null_ptr, c_null_ptr, c_loc(eig_vals), evecs, neig_found)
+  !! Elpa :
+  ! error = Heev_Elpa(Matrix, eig_vals, evecs, Grows, 2, c_null_ptr, 1);
+  !!!!!!
+  !! incase it is standard non-TDA-
+  ! error = BSE_Solver(Matrix, c_null_ptr, c_null_ptr, eig_vals, evecs, neigs_found)
+  ! Elpa:
+  ! error = BSE_Solver_Elpa(Matrix, eig_vals, evecs, eig_vals, evecs, neigs_found)
+
+  ! The first neig_found entries of the eig_vals are filled with the eigenvalues
+
   if (my_rank == 0) then
-    do i = 0, Grows - 1
+    do i = 1, neig_found 
       print *, real(eig_vals(i)), aimag(eig_vals(i))
     end do
   end if
-#else
-  if (my_rank == 0) then
-    logical :: pass
-    pass = .true.
-    do i = 0, Grows - 1
-      if (abs(real(eig_vals(i)) - refvals(i)) > 1.0e-5_C_FLOAT .or. &
-          abs(aimag(eig_vals(i))) > 1.0e-8_C_FLOAT) then
-        pass = .false.
-        exit
-      end if
-    end do
 
-    if (pass) then
-      print *, "Passed ;)"
-    else
-      print *, "Failed ;)"
-    end if
-  end if
-#endif
-
-  deallocate(eig_vals)
-  call free_D_Matrix(Matrix_Z)
+  ! Free the initial matrix to freeup some space for eigen-vectors
   call free_D_Matrix(Matrix)
+
+  ! Eigenvalues are stored in columns of the distributed matrix i.e 
+  ! evecs[:,i] represents the ith eigenvector
+  
+  ! Now we Retrieve the components of the eigenvectors in which ever distrbutation you 
+  ! want. For example I want to distribute the number of eigenvectors such that 
+  ! each cpu has subset of full vectors i.e [:,neig_found/ncpus].
+  ! This can be done by the get Queue
+  
+  
+  ele_this_cpu = neig_found/size !CEILING(real(neig_found)/real(ncpus)) 
+  shift  = ele_this_cpu * my_rank
+  ele_rem = mod(neig_found, size)
+  if (my_rank < ele_rem) then
+    ele_this_cpu = ele_this_cpu + 1
+    shift = shift + my_rank
+  else 
+    shift = shift + ele_rem
+  endif
+  
+  allocate(eig_vectors(Grows,ele_this_cpu))
+  
+  eig_vectors = 0
+
+  ! // Now lets us start the retrvel procedure
+
+  ! First and foremost initiate the getQueue
+  error = initiateGetQueue(evecs, ele_this_cpu*grows)
+
+  ! Check the error
+  if (error /= 0) then
+    call MPI_Abort(MPI_COMM_WORLD, 1, ierr)
+  end if
+  
+  do jc = shift+1, shift+ele_this_cpu
+    do ir = 1, grows
+      error = dmatget_fortran(evecs, ir,jc, c_loc(eig_vectors(ir,jc)))
+      if (error /= 0) then
+        print *, error
+        call mpi_abort(mpi_comm_world, 1, ierr)
+      end if
+    enddo
+  enddo
+  
+  ! Finalize the get queue
+  error = ProcessGetQueue(evecs)
+  if (error /= 0) then
+    call MPI_Abort(MPI_COMM_WORLD, 1, ierr)
+  end if
+  
+  ! if (my_rank == 0) then
+  !   do jc = shift+1, shift+ele_this_cpu
+  !     do ir = 1, grows
+  !       print *, eig_vectors(ir,jc)
+  !     enddo
+  !   enddo
+  ! endif 
+
+  
+  if (my_rank == 0) then
+    print *, 'Printing Norms'
+    do jc = shift+1, shift+ele_this_cpu
+        print *, sqrt( sum(abs(eig_vectors(:,jc)**2 )))
+    enddo
+  endif 
+
+
+  ! Free all the matrices 
+  call free_D_Matrix(evecs)
   call BLACScxtFree(mpicxt)
+
+  deallocate(eig_vectors)
+  deallocate(eig_vals)
 
   call MPI_Finalize(ierr)
 
